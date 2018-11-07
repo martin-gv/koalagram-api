374094d0ed8b27ef5ce5abd9c9e4f4c20e055340
Martin Guardado <martinguardado@gmail.com>
2018-11-06 23:59:49 -0700

    Enable sample data api endpoint

--- handlers/sampleData.js
+++ handlers/sampleData.js
@@ -17,17 +17,17 @@ exports.getUnsplashImages = async (req, res, next) => {
     //  const { searchTerm, numPages, startPage } = req.body;
     //  done: portrait 4 pages, start 1; animals 40 pages, start 1
     //  done: portrait 4 pages, start 5; animals 40 pages, start 41
-    const searchTerm = "animals",
-      numPages = 40,
-      startPage = 41;
+    const searchTerm = "portrait",
+      numPages = 4,
+      startPage = 1;
     if (searchTerm && numPages && startPage) {
       const requests = [];
       for (var i = startPage; i <= startPage + numPages - 1; i++) {
-        //   requests.push(
-        //     axios.get("https://api.unsplash.com/search/photos/", {
-        //       params: { query: searchTerm, per_page: 30, page: i }
-        //     })
-        //   );
+          requests.push(
+            axios.get("https://api.unsplash.com/search/photos/", {
+              params: { query: searchTerm, per_page: 30, page: i }
+            })
+          );
       }
       const resolvedPromises = await Promise.all(requests);
       const resultsArr = resolvedPromises.map(x => x.data.results);
@@ -42,13 +42,13 @@ exports.getUnsplashImages = async (req, res, next) => {
       ]);
       const sql =
         "INSERT INTO images (type, raw_url, full_url, regular_url, small_url, thumb_url) VALUES ?";
-      // db.query(sql, [insertData], (err, result) => {
-      //   if (err) {
-      //     next(err);
-      //   } else {
-      //     res.status(200).json({ result });
-      //   }
-      // });
+      db.query(sql, [insertData], (err, result) => {
+        if (err) {
+          next(err);
+        } else {
+          res.status(200).json({ result });
+        }
+      });
     } else {
       next({
         message: "Required parameters are searchTerm, numPages, and startPage"
