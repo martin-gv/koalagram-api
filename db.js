5bd807582543c117d869042648c8dbc3b8609b1c
Martin Guardado <martinguardado@gmail.com>
2018-11-07 22:16:57 -0700

    Change connection to remote database

--- db.js
+++ db.js
@@ -1,19 +1,12 @@
 const mysql = require("mysql");
 
-// const connection = mysql.createConnection({
-//   host: "us-cdbr-iron-east-01.cleardb.net",
-//   user: "b57e642d15cd4c",
-//   password: "76ca1458",
-//   database: "heroku_d169760d6be1801"
-// });
-
-// const connection = mysql.createPool({
-//   connectionLimit: 10,
-//   host: "us-cdbr-iron-east-01.cleardb.net",
-//   user: "b57e642d15cd4c",
-//   password: "76ca1458",
-//   database: "heroku_d169760d6be1801"
-// });
+const connection = mysql.createPool({
+  connectionLimit: 10,
+  host: "us-cdbr-iron-east-01.cleardb.net",
+  user: "b57e642d15cd4c",
+  password: "76ca1458",
+  database: "heroku_d169760d6be1801"
+});
 
 // const connection = mysql.createConnection({
 //   host: "localhost",
@@ -22,13 +15,13 @@ const mysql = require("mysql");
 //   database: "koalagram"
 // });
 
-const connection = mysql.createPool({
-  connectionLimit: 10,
-  host: "localhost",
-  user: "root",
-  password: "password",
-  database: "koalagram"
-});
+// const connection = mysql.createPool({
+//   connectionLimit: 10,
+//   host: "localhost",
+//   user: "root",
+//   password: "password",
+//   database: "koalagram"
+// });
 
 // connection.connect(err => {
 //   if (err) {
