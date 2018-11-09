6c11c8b8976595aa30ea954fddcb615afd96ffb0
Martin Guardado <martinguardado@gmail.com>
2018-11-08 20:52:49 -0700

    Testing using original db settings

--- db.js
+++ db.js
@@ -1,25 +1,33 @@
 const mysql = require("mysql");
 
-let connectionOptions = {};
+// let connectionOptions = {};
 
-if (process.env.HOST && process.env.HOST === "localhost") {
-  connectionOptions = {
-    connectionLimit: 100,
-    host: "localhost",
-    user: "root",
-    password: "password",
-    database: "koalagram"
-  };
-} else {
-  connectionOptions = {
-    connectionLimit: 100,
-    host: "us-cdbr-iron-east-01.cleardb.net",
-    user: "b57e642d15cd4c",
-    password: "76ca1458",
-    database: "heroku_d169760d6be1801"
-  };
-}
+// if (process.env.HOST && process.env.HOST === "localhost") {
+//   console.log("localhost db");
+//   connectionOptions = {
+//     connectionLimit: 100,
+//     host: "localhost",
+//     user: "root",
+//     password: "password",
+//     database: "koalagram"
+//   };
+// } else {
+//   console.log("cloud db");
+//   connectionOptions = {
+//     connectionLimit: 100,
+//     host: "us-cdbr-iron-east-01.cleardb.net",
+//     user: "b57e642d15cd4c",
+//     password: "76ca1458",
+//     database: "heroku_d169760d6be1801"
+//   };
+// }
 
-const connection = mysql.createPool(connectionOptions);
+const connection = mysql.createPool({
+  connectionLimit: 100,
+  host: "us-cdbr-iron-east-01.cleardb.net",
+  user: "b57e642d15cd4c",
+  password: "76ca1458",
+  database: "heroku_d169760d6be1801"
+});
 
 module.exports = connection;
