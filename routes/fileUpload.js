e7c5ac9bde546276f60b0a4f966463dfae90c94c
Martin Guardado <martinguardado@gmail.com>
2018-11-10 23:22:35 -0700

    New API endpoint: get signed reques from S3 for direct upload

--- routes/fileUpload.js
+++ routes/fileUpload.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const aws = require("aws-sdk");
 const router = express.Router();
 
 const upload = require("../services/fileUpload");
@@ -8,6 +9,33 @@ router.post("/", function(req, res, next) {
   singleUpload(req, res, function(err) {
     if (err) return next(err);
     return res.json({ imageUrl: req.file.location });
+  });
+});
+
+// get signature for uploading to S3
+router.get("/", function(req, res, next) {
+  aws.config.region = "us-east-2";
+  const s3 = new aws.S3();
+  const fileName = req.query.fileName;
+  const fileType = req.query.fileType;
+  const s3Params = {
+    Bucket: process.env.S3_BUCKET,
+    Key: fileName,
+    Expires: 60,
+    ContentType: fileType,
+    ACL: "public-read"
+  };
+
+  s3.getSignedUrl("putObject", s3Params, (err, data) => {
+    if (err) {
+      console.log(err);
+      return next(err);
+    }
+    const returnData = {
+      signedRequest: data,
+      url: `https://${process.env.S3_BUCKET}.s3.amazonaws.com/${fileName}`
+    };
+    res.status(200).json(returnData);
   });
 });
 
